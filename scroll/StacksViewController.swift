1038a7da68f42382e67a22c6eec3a634e950f0c4
mike <mike@guidebook.com>
2015-07-16 11:31:57 -0700

    obviously this code sucks but is this what you were trying to do?

--- scroll/StacksViewController.swift
+++ scroll/StacksViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class StacksViewController: UIViewController, UINavigationControllerDelegate {
-
+class StacksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UINavigationControllerDelegate {
+    
     var btn_stacks : UIButton!
     var stroke_stacks : UIView!
     var chevron : UIImageView!
@@ -19,49 +19,67 @@ class StacksViewController: UIViewController, UINavigationControllerDelegate {
         super.viewDidLoad()
         
         self.title = "Stacks"
-
+        
         // SETUP VIEW
         view.backgroundColor = UIColor.neutralColor(alpha: 1.0)
         
-        
-        // COMPOSE
-        
-        btn_stacks = UIButton(frame: CGRect(x: 0, y: 64, width: screenSize.width, height: 100))
-        btn_stacks.setTitle("Stack", forState: .Normal)
-        btn_stacks.contentHorizontalAlignment = UIControlContentHorizontalAlignment.Left
-        btn_stacks.titleEdgeInsets = UIEdgeInsetsMake(0, 20, 0, 0)
-        btn_stacks.backgroundColor = UIColor.clearColor()
-        btn_stacks.addTarget(self, action: "didTapStacks:", forControlEvents: .TouchUpInside)
-        btn_stacks.titleLabel!.font = UIFont.tertiaryFont()
-        btn_stacks.setTitleColor(UIColor.primaryColor(alpha: 1.0), forState: UIControlState.Normal)
-        view.addSubview(btn_stacks)
-        
-        // STROKE COMPOSE
-        
-        stroke_stacks = UIView(frame: CGRect(x: 20, y: 100, width: screenSize.width - 40, height:1))
-        stroke_stacks.backgroundColor = UIColor.strokeColor(alpha: 1.0)
-        btn_stacks.addSubview(stroke_stacks)
-
-        
-        // CHEVRON
-        
-        chevron = UIImageView(image: UIImage(named: "chevron"))
-        chevron.frame = CGRect(x: screenSize.width - 28, y: 44, width: 8.0, height: 13.0)
-        btn_stacks.addSubview(chevron)
-   
-        
-
-        // Do any additional setup after loading the view.
+        let tableView = UITableView(frame: self.view.bounds)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .None
+        self.view.addSubview(tableView)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func didTapStacks(sender:UIButton){
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
         let homeVC: HomeViewController = HomeViewController(nibName: nil, bundle: nil)
         self.navigationController?.pushViewController(homeVC, animated: true)
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        // EW REFACTOR ME
+        // XXX TODO
+        let cell: AnyObject? = tableView.dequeueReusableCellWithIdentifier("StacksCell")
+        if let cell = cell as? UITableViewCell {
+            return cell
+        } else {
+            let cell = UITableViewCell(frame: CGRect(x: 0, y: 64, width: screenSize.width, height: 100))
+            cell.textLabel?.text = "Stack"
+            //           cell.contentHorizontalAlignment = UIControlContentHorizontalAlignment.Left
+            //            cell.titleEdgeInsets = UIEdgeInsetsMake(0, 20, 0, 0)
+            cell.backgroundColor = UIColor.clearColor()
+            cell.textLabel?.font = UIFont.tertiaryFont()
+            cell.textLabel?.textColor = UIColor.primaryColor(alpha: 1.0)
+            //            cell.setTitleColor(UIColor.primaryColor(alpha: 1.0), forState: UIControlState.Normal)
+            //           cell.addSubview(btn_stacks)
+            
+            // STROKE COMPOSE
+            
+            stroke_stacks = UIView(frame: CGRect(x: 20, y: 100, width: screenSize.width - 40, height:1))
+            stroke_stacks.backgroundColor = UIColor.strokeColor(alpha: 1.0)
+            cell.addSubview(stroke_stacks)
+            
+            
+            // CHEVRON
+            
+            chevron = UIImageView(image: UIImage(named: "chevron"))
+            chevron.frame = CGRect(x: screenSize.width - 28, y: 44, width: 8.0, height: 13.0)
+            cell.addSubview(chevron)
+            return cell
+        }
+    }
 }
