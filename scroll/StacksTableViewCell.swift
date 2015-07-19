b6f59a1452f8a50bc90c1055b7628c8b49e0e0c1
Ed Chao <eddchao@gmail.com>
2015-07-18 17:11:53 -0700

    class organization

---
+++ scroll/StacksTableViewCell.swift
@@ -0,0 +1,62 @@
+//
+//  StacksTableViewCell.swift
+//  scroll
+//
+//  Created by Ed Chao on 7/18/15.
+//  Copyright (c) 2015 Ed Chao. All rights reserved.
+//
+
+import UIKit
+
+class StacksTableViewCell: UITableViewCell {
+
+    var label_text : UILabel! = UILabel()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.contentView.backgroundColor = UIColor.neutralColor(alpha: 1.0)
+        
+        // LABEL
+        label_text = UILabel(frame: CGRectMake(30, 30, self.bounds.size.width, 25))
+        label_text.textColor = UIColor.primaryColor(alpha: 1)
+        label_text.font = UIFont.tertiaryFont()
+        label_text.numberOfLines = 0
+        self.contentView.addSubview(label_text)
+        
+        
+        self.layout()
+        
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+    }
+    
+    func layout(){
+        
+        self.label_text.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        
+        let viewsDictionary = [
+            "label_text" :label_text,
+        ]
+        
+        // POSITIONS
+        
+        let view_constraint_H:NSArray = NSLayoutConstraint.constraintsWithVisualFormat("H:|-20-[label_text]-20-|", options: NSLayoutFormatOptions.AlignAllLeading, metrics: nil, views: viewsDictionary)
+        
+        let view_constraint_V:NSArray = NSLayoutConstraint.constraintsWithVisualFormat("V:|-30-[label_text]-30-|", options: NSLayoutFormatOptions.AlignAllLeading, metrics: nil, views: viewsDictionary)
+        
+        self.contentView.addConstraints(view_constraint_H as [AnyObject])
+        self.contentView.addConstraints(view_constraint_V as [AnyObject])
+        
+        
+    }
+
+}
